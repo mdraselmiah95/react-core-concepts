a48b1753ca4319d49225de4063fd504447f3ebc2
rasel <russelroy15@gmail.com>
2021-09-18 14:17:03 +0600

    WIP on React core concept

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import "./App.css";
 import logo from "./logo.svg";
 
+const newNumber = 3488;
+const singer = {
+  name: "Asif",
+  job: "singer",
+};
+const singerTwo = {
+  name: "Singh",
+  job: "singer",
+};
+
+// console.log(newNumber, singer);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h3>This is the start of React</h3>
         <p>Happy journey with React.</p>
-        <div className="container">EveryThing will be grate again.</div>
+        <div className="container">
+          <h3>EveryThing will be grate again.</h3>
+          <p>My number is : {newNumber}</p>
+          <p>Singer: {singer.name + " " + singer.job}</p>
+          <p>Real singer: {singerTwo.name}</p>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
